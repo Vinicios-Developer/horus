f5f8be02d7034e152cb82a4decc9806372cd3f25
Vinicios-Developer <viniciosgabriel_18@hotmail.com>
2023-07-01 00:38:55 -0400

    Atualização do código referente a Pedidos

--- src/routers/rotas_pedidos.py
+++ src/routers/rotas_pedidos.py
@@ -8,30 +8,30 @@ from src.schemas.schemas import Pedido
 router = APIRouter()
 
 
-@router.post('/pedidos', status_code=status.HTTP_201_CREATED)
+@router.post('/pedidos', status_code=status.HTTP_201_CREATED, response_model=Pedido)
 def fazer_pedido(pedido: Pedido, session: Session = Depends(get_db)):
     pedido_criado = RepositorioPedido(session).gravar_pedido(pedido)
     return pedido_criado
 
 
-@router.get('/pedidos/{id}')
+@router.get('/pedidos/{id}', response_model=Pedido)
 def exibir_pedido(id: int, session: Session = Depends(get_db)):
     try:
         pedido_localizado = RepositorioPedido(session).buscar_por_id(id)
         return pedido_localizado
     except:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'Não existe um pedido com id={id}')
+                            detail=f'Pedido {id} inexistente')
 
 
-@router.get('/pedidos', response_model=List[Pedido])
+@router.get('/pedidos/{usuario_id}/compras', response_model=List[Pedido])
 def listar_pedidos(usuario_id: int, session: Session = Depends(get_db)):
     pedidos = RepositorioPedido(
         session).listar_meus_pedidos_por_usuario_id(usuario_id)
     return pedidos
 
 
-@router.get('/pedidos/{usuario_id}/vendas', response_model=List[Pedido])
+@router.get('/pedidos/{usuario_id}/vendas')
 def listar_vendas(usuario_id: int, session: Session = Depends(get_db)):
     pedidos = RepositorioPedido(
         session).listar_minhas_vendas_por_usuario_id(usuario_id)
