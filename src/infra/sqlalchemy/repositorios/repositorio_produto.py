2ecd90ea4647c5c19dd45e606194f10ab7091e7d
Vinicios-Developer <viniciosgabriel_18@hotmail.com>
2023-07-04 14:16:10 -0400

    Autenticação para produtos

--- src/infra/sqlalchemy/repositorios/repositorio_produto.py
+++ src/infra/sqlalchemy/repositorios/repositorio_produto.py
@@ -9,27 +9,30 @@ class RepositorioProduto():
     def __init__(self, db: Session):
         self.session = db
 
-    def criar(self, produto: schemas.Produto):
+    def criar(self, produto: schemas.Produto, usuario_id: int):
+        models.Produto.usuario_id = usuario_id
         db_produto = models.Produto(nome=produto.nome,
                                     detalhes=produto.detalhes,
                                     preco=produto.preco,
                                     disponivel=produto.disponivel,
                                     tamanho=produto.tamanho,
-                                    usuario_id=produto.usuario_id)
+                                    usuario_id=usuario_id)
         self.session.add(db_produto)
         self.session.commit()
         self.session.refresh(db_produto)
         return db_produto
 
-    def listar(self):
-        produtos = self.session.query(models.Produto).all()
+    def listar(self, usuario_id: int):
+        consulta = select(models.Produto).where(
+            models.Produto.usuario_id == usuario_id)
+        produtos = self.session.execute(consulta).scalars().all()
         return produtos
-    
-    def buscarPorId(self, id: int):
-        consulta = select(models.Produto).where(models.Produto.id == id)
+
+    def buscarPorId(self, produto_id: int, usuario_id: int):
+        consulta = select(models.Produto).where(models.Produto.usuario_id == usuario_id, models.Produto.id == produto_id)
         produto = self.session.execute(consulta).first()
         return produto
-    
+
     def editar(self, id: int, produto: schemas.Produto):
         update_stmt = update(models.Produto).where(
             models.Produto.id == id).values(nome=produto.nome,
--- src/routers/rotas_produtos.py
+++ src/routers/rotas_produtos.py
@@ -1,28 +1,30 @@
 from fastapi import APIRouter, status, Depends, HTTPException
 from typing import List
-from src.schemas.schemas import Produto, ProdutoSimples
+from src.schemas.schemas import Produto, ProdutoSimples, Usuario
 from sqlalchemy.orm import Session
 from src.infra.sqlalchemy.config.database import get_db
 from src.infra.sqlalchemy.repositorios.repositorio_produto import RepositorioProduto
+from src.routers.auth_utils import obter_usuario_logado
 
 router = APIRouter()
 
 
 @router.post('/produtos', status_code=status.HTTP_201_CREATED, response_model=ProdutoSimples)
-def criar_produto(produto: Produto, db: Session = Depends(get_db)):
-    produto_criado = RepositorioProduto(db).criar(produto)
+def criar_produto(produto: Produto, usuario: Usuario = Depends(obter_usuario_logado), db: Session = Depends(get_db)):
+    produto_criado = RepositorioProduto(db).criar(produto, usuario.id)
     return produto_criado
 
 
 @router.get('/produtos', status_code=status.HTTP_200_OK, response_model=List[ProdutoSimples])
-def listar_produtos(db: Session = Depends(get_db)):
-    produtos = RepositorioProduto(db).listar()
+def listar_produtos(usuario: Usuario = Depends(obter_usuario_logado), db: Session = Depends(get_db)):
+    produtos = RepositorioProduto(db).listar(usuario.id)
     return produtos
 
 
 @router.get('/produtos/{id}')
-def exibir_produto(id: int, session: Session = Depends(get_db)):
-    produto_localizado = RepositorioProduto(session).buscarPorId(id)
+def exibir_produto(id: int, usuario: Usuario = Depends(obter_usuario_logado), session: Session = Depends(get_db)):
+    produto_localizado = RepositorioProduto(
+        session).buscarPorId(id, usuario.id)
     if produto_localizado is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Produto não encontrado com o id {id}")
